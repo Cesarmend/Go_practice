ca01429cf6dcc82f5a85b0040196260dc5586c35
Cesar <mendezcesarpablo@gmail.com>
2022-07-21 00:14:42 -0500

    Go variables

--- hellow.go
+++ hellow.go
@@ -5,6 +5,33 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
+	// Declaracion de constantes
+	const pi float64 = 3.14
+	const pi2 = 3.1415
+
+	fmt.Println("pi: ", pi)
+	fmt.Println("pi2: ", pi2)
+
+	//Declaracion de variables enteras
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	//Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	// Area de cuadrado
+
+	const baseCuadraro = 10
+	areaCuadrado := baseCuadraro * baseCuadraro
+
+	fmt.Println("Area cuadrado= ", areaCuadrado)
 
 }
